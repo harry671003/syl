e9c304567035ad557d92ded01881d7a6ddc1091d
Harry John <harry671003@gmail.com>
2018-01-20 22:14:47 +0530

    Added bodyparser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const config = require('./config');
 const logging = require('./app/infrastructure/logging');
@@ -6,6 +7,10 @@ const logging = require('./app/infrastructure/logging');
 const initializeRoutes = require('./app/routes');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 initializeRoutes(app, config, logging.createLogger(config));
 
 app.listen(config.environment.port);
