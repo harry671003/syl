c3295498f7424edcb5b61eedd929736034e2729b
Harry John <harry671003@gmail.com>
2018-01-20 12:10:03 +0530

    Changed to simple js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (request, response) => response.send('Hello World'));
+app.get('/', function(request, response) {
+	response.send('Hello World')
+});
 
-app.listen(port, () => console.log('Listening on port 3000'));
+app.listen(port, function() {
+	console.log('Listening on port 3000')
+});
